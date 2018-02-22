a2c04bad32d025b2daba2b3f2091f0a1579deade
Mauro Franceschini <mauro.franceschini@cedac.com>
2018-02-22 16:40:04 +0100

    Added domain object for users.

---
+++ internal/app/domain/model/enablement.go
@@ -0,0 +1,45 @@
+package model
+
+import (
+	"errors"
+	"time"
+)
+
+// Enablement is the value object used to provide data about User enable/disable status.
+//
+// Its default value (a disabled indefinite enablement) is a valid value object and can be used as is.
+type Enablement struct {
+	Enabled   bool
+	StartDate time.Time
+	EndDate   time.Time
+}
+
+// IndefiniteEnablement will make a new indefinite enabled-status enablement.
+func IndefiniteEnablement() Enablement {
+	return Enablement{Enabled: true}
+}
+
+// MakeEnablement will make a new enablement with supplied status and start and end time.
+// The function will fail with an error if the startDate is greater than endDate or if only one of startDate
+// and endDate are provided.
+func MakeEnablement(enabled bool, startDate, endDate time.Time) (Enablement, error) {
+	if startDate.After(endDate) {
+		return Enablement{}, errors.New("Invalid startDate and endDate provided")
+	}
+	return Enablement{enabled, startDate, endDate}, nil
+}
+
+// ActuallyEnabled will check if the enablement is actually enabled.
+func (e Enablement) ActuallyEnabled() bool {
+	return e.Enabled && !e.TimeExpired()
+}
+
+// TimeExpired will check if the validity time of this enablement is expired.
+func (e Enablement) TimeExpired() bool {
+	expired := false
+	if !e.StartDate.IsZero() && !e.EndDate.IsZero() {
+		now := time.Now()
+		expired = now.After(e.StartDate) && now.Before(e.EndDate)
+	}
+	return expired
+}
---
+++ internal/app/domain/model/person.go
@@ -0,0 +1,72 @@
+package model
+
+import (
+	"github.com/maurofran/kit/assert"
+)
+
+// ContactInformation is the value object used in order to provide content information for a person.
+type ContactInformation struct {
+	PostalAddress      PostalAddress
+	EmailAddress       EmailAddress
+	PrimaryTelephone   Telephone
+	SecondaryTelephone Telephone
+}
+
+// MakeContactInformation will create a new contact information structure with supplied parameters.
+func MakeContactInformation(postalAddress PostalAddress, emailAddress EmailAddress,
+	primaryTelephone, secondaryTelephone Telephone) (ContactInformation, error) {
+	if err := assert.NotZero(emailAddress, "emailAddress"); err != nil {
+		return ContactInformation{}, err
+	}
+	return ContactInformation{postalAddress, emailAddress, primaryTelephone, secondaryTelephone}, nil
+}
+
+// IsZero will check if supplied contact information is zero value.
+func (ci ContactInformation) IsZero() bool {
+	return ci.PostalAddress.IsZero() && ci.EmailAddress.IsZero() && ci.PrimaryTelephone.IsZero() &&
+		ci.SecondaryTelephone.IsZero()
+}
+
+// Person is an entity used in order to provide data about a person.
+type Person struct {
+	Name               FullName
+	ContactInformation ContactInformation
+}
+
+// NewPerson will create a new person for supplied data.
+func NewPerson(fullName FullName, contactInformation ContactInformation) (*Person, error) {
+	if err := assert.NotZero(fullName, "fullName"); err != nil {
+		return nil, err
+	}
+	if err := assert.NotZero(contactInformation, "contactInformation"); err != nil {
+		return nil, err
+	}
+	return &Person{fullName, contactInformation}, nil
+}
+
+// EmailAddress will retrieve the e-mail address of person.
+func (p *Person) EmailAddress() EmailAddress {
+	return p.ContactInformation.EmailAddress
+}
+
+func (p *Person) changeContactInformation(contactInformation ContactInformation) (bool, error) {
+	if err := assert.NotZero(contactInformation, "contactInformation"); err != nil {
+		return false, err
+	}
+	if p.ContactInformation != contactInformation {
+		p.ContactInformation = contactInformation
+		return true, nil
+	}
+	return false, nil
+}
+
+func (p *Person) changeName(fullName FullName) (bool, error) {
+	if err := assert.NotZero(fullName, "fullName"); err != nil {
+		return false, err
+	}
+	if p.Name != fullName {
+		p.Name = fullName
+		return true, nil
+	}
+	return false, nil
+}
