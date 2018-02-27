d7700cf8c05fbfc9a853ad02df27b114a090b3af
Mauro Franceschini <mauro.franceschini@cedac.com>
2018-02-27 13:32:59 +0100

    Basic setup of command line.

--- cmd/iam/main.go
+++ cmd/iam/main.go
@@ -1,5 +1,58 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+var mongoDB string
+
 func main() {
 
+	app := cli.NewApp()
+	app.Name = "iam"
+	app.Usage = "Identity and Access Manager CLI"
+	app.Version = "1.0.0"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "dburl",
+			Value:       "mongodb://localhost:27017/iam_db",
+			Usage:       "Connect to MongoDB at `URL`",
+			Destination: &mongoDB,
+			EnvVar:      "DB_URL",
+		},
+	}
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:        "tenant",
+			Usage:       "Run a command on specific tenant",
+			Aliases:     []string{"t"},
+			Subcommands: []cli.Command{},
+		},
+		cli.Command{
+			Name:        "user",
+			Usage:       "Run a command on specific user",
+			Aliases:     []string{"u"},
+			Subcommands: []cli.Command{},
+		},
+		cli.Command{
+			Name:        "group",
+			Usage:       "Run a command on specific group",
+			Aliases:     []string{"g"},
+			Subcommands: []cli.Command{},
+		},
+		cli.Command{
+			Name:        "role",
+			Usage:       "Run a command on specific role",
+			Aliases:     []string{"r"},
+			Subcommands: []cli.Command{},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- cmd/iamd/main.go
+++ cmd/iamd/main.go
@@ -1,5 +1,54 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/pkg/errors"
+	cli "gopkg.in/urfave/cli.v1"
+	db "upper.io/db.v3/mongo"
+)
+
+var mongoDB string
+
 func main() {
 
+	app := cli.NewApp()
+	app.Name = "iamd"
+	app.Usage = "Identity and Access Manager Daemon"
+	app.Version = "1.0.0"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "dburl",
+			Value:       "mongodb://localhost:27017/iam_db",
+			Usage:       "Connect to MongoDB at `URL`",
+			Destination: &mongoDB,
+			EnvVar:      "DB_URL",
+		},
+	}
+	app.Action = startDaemon
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startDaemon(c *cli.Context) error {
+	url, err := db.ParseURL(mongoDB)
+	if err != nil {
+		return errors.Wrapf(err, "Error occurred while parsing URL %s", mongoDB)
+	}
+	session, err := db.Open(url)
+	if err != nil {
+		return errors.Wrapf(err, "Error occurred while opening connection to %s", mongoDB)
+	}
+	defer session.Close()
+
+	//	tenantRepo := &mongo.TenantRepository{Database: session}
+	//	userRepo := &mongo.UserRepository{Database: session}
+	//	groupRepo := &mongo.GroupRepository{Database: session}
+	//	roleRepo := &mongo.RoleRepository{Database: session}
+
+	return nil
 }
